7c43ad32fd8ded2a4d154c54ae3f5407bec42726
Javier Cáceres Bordallo <kaceresdev@gmail.com>
2023-09-07 20:11:22 +0200

    No redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-];
+const routes: Routes = [{ path: '', component: HomeComponent, pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
