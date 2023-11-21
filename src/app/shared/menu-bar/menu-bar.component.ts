db6131cb596c6d5a984436181dd4053e3ba11cf9
Javier Cáceres Bordallo <kaceresdev@gmail.com>
2023-11-21 22:32:41 +0100

    Upgrade

--- src/app/shared/menu-bar/menu-bar.component.ts
+++ src/app/shared/menu-bar/menu-bar.component.ts
@@ -6,19 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu-bar.component.scss'],
 })
 export class MenuBarComponent {
+  menuItems = [
+    {
+      title: 'Home',
+      route: '/',
+    },
+    {
+      title: 'Contacto',
+      route: '/contact',
+    },
+    {
+      title: 'Blog',
+      route: '/',
+    },
+    {
+      title: 'Conóceme más',
+      route: '/',
+    },
+  ];
+
   toggleNavIcon() {
     const nav = document.getElementById('nav-icon');
     const menu = document.getElementById('menu-items');
     nav!.classList.toggle('open');
     menu!.classList.toggle('show');
+    menu!.style.display = menu!.style.display === 'block' ? 'none' : 'block';
+
+    // Evitar el desplazamiento del contenido de la página
+    document.body.style.overflow = menu!.style.display === 'block' ? 'hidden' : 'auto';
   }
 
   easterEgg() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-    const logo = document.getElementById('logo');
-    logo!.classList.add('spinner-logo');
-    setTimeout(() => {
-      logo!.classList.remove('spinner-logo');
-    }, 500);
+    if (!document.getElementById('menu-items')?.classList.contains('show')) {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+      const logo = document.getElementById('logo');
+      logo!.classList.add('spinner-logo');
+      setTimeout(() => {
+        logo!.classList.remove('spinner-logo');
+      }, 500);
+    }
   }
 }
